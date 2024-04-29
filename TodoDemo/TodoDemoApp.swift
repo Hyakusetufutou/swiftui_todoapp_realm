a68577c9c4cb5ea40c1e00863446da65c034aa86
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-04-30 08:33:08 +0900

    first commit

---
+++ TodoDemo/TodoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoDemoApp.swift
+//  TodoDemo
+//  
+//  
+//
+
+import SwiftUI
+
+@main
+struct TodoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
